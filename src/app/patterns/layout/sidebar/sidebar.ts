8b95556c8bedac95d48c6cecf362f03567cebc6c
bagel <ponomarenko.pavel.web@gmail.com>
2025-09-13 14:31:42 +0300

    add close button for menu, update styles

--- src/app/patterns/layout/sidebar/sidebar.ts
+++ src/app/patterns/layout/sidebar/sidebar.ts
@@ -1,16 +1,23 @@
-import {Component} from '@angular/core';
+import {Component, inject} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {Menu} from 'primeng/menu';
 import {MenuItem} from 'primeng/api';
 import {MENU_CONFIG} from '@core/configs/menu';
+import {Button} from 'primeng/button';
+import {LayoutService} from '@core/services/layout.service';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [CommonModule, Menu],
+  imports: [CommonModule, Menu, Button],
   templateUrl: './sidebar.html',
   styleUrl: './sidebar.scss'
 })
 export class Sidebar {
   protected readonly items: MenuItem[] = MENU_CONFIG;
+  layoutService = inject(LayoutService);
+
+  toggleMenu(): void {
+    this.layoutService.toggleMenu();
+  }
 }
