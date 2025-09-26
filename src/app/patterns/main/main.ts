2c620e888edb1b1e51b2f152ef8a1f3ab185d0e3
bagel <ponomarenko.pavel.web@gmail.com>
2025-09-26 19:00:27 +0300

    update translations

--- src/app/patterns/main/main.ts
+++ src/app/patterns/main/main.ts
@@ -1,10 +1,12 @@
 import {Component} from '@angular/core';
 import {RouterLink} from '@angular/router';
+import {TranslocoPipe} from '@ngneat/transloco';
 
 @Component({
   selector: 'app-main',
   imports: [
     RouterLink,
+    TranslocoPipe,
   ],
   templateUrl: './main.html',
   styleUrl: './main.scss'
